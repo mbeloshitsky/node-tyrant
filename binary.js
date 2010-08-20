87635517f792f050fb60d31fae5894defaffcbe0
Michel Beloshitsky <mbeloshitsky@gmail.com>
2010-08-21 05:58:34 +0600

    New stream object ready. Last state before refactor.

--- binary.js
+++ binary.js
@@ -8,25 +8,28 @@
  */
 
 /**
- * Basic idea of this format functions borrowed from "binary format" 
+ * Basic idea of this format functions borrowed from "binary format"
  * routine of tcl standart library. But abbreviations now did not match.
  *
- * This implementation of binary routines is not full; it's tuned for 
+ * This implementation of binary routines is not full; it's tuned for
  * tokyo tyrant connector.
  *
  * Abbreviations:
  *
- * b - byte (octet).
- * I - 32-bit big-endian integer
- * S - string (sequence of chars) Only avaible in "format" function.
+ * b - byte (octet).  
+ * i - 32-bit big-endian integer 
+ * S - string (sequence of chars).  
+ *
+ * In read function "S" means pascal string. In mean that at first
+ * read length (N) from 32-integer. After that N bytes read into string.
  *
  * Example:
  *
- * > require('./binary').format('bbIIS', 1, 2, 4, 5, 'test') 
+ * > require('./binary').format('bbIIS', 1, 2, 4, 5, 'test')
  *
  * produces:
  *
- * > 0x01 0x02 0x00 0x00  0x00 0x04 0x00 0x00  
+ * > 0x01 0x02 0x00 0x00  0x00 0x04 0x00 0x00
  * > 0x00 0x05 0x74 0x65  0x73 0x74
  *
  * > require('./binary').read(formatString, binaryStringToParse)
@@ -40,7 +43,7 @@ exports.format = function () {
       switch (spec) {
       case 'b': out += String.fromCharCode(args[argi])
          break
-      case 'I':
+      case 'i':
          var iv = args[argi], i=4, ov = []
          while (i--) {
             ov.push(String.fromCharCode(iv & 0xFF))
@@ -58,23 +61,38 @@ exports.format = function () {
    return out
 }
 
-exports.read = function (fmtStr, str) {
-   var out = [], i = 0
-   fmtStr.split('').map(function (spec) {
-      switch (spec) {
-      case 'b': 
-	 out.push(str.charCodeAt(i)); i++
+exports.read = function read (fmtStr, reader, cb) {
+   var out = [], spec = fmtStr.split('')
+
+   function next(specCh) {
+      if (!specCh)
+	 return cb(out)
+
+      switch (specCh) {
+      case 'b':
+         reader.read(1, function (str) {
+            out.push(str.charCodeAt(0));
+            next(spec.shift())
+         })
          break
-      case 'I':
-         var j = -1, v = 0
-	 while (++j < 4) {
-	    v += (str.charCodeAt(i + j) & 0xFF) << (8 * (3 - j))
-	 }
-	 out.push(v); i += 4
+      case 'i':
+         reader.read(4, function (str) {
+            var j = -1, v = 0
+            while (++j < 4) {
+               v += (str.charCodeAt(j) & 0xFF) << (8 * (3 - j))
+            }
+            out.push(v)
+         })
          break
-      default:
-         throw 'Binary read syntax error'
+      case 'S':
+         read('i', reader, function (len) {
+            reader.read(len[0], function (data) {
+               out.push(data)
+            })
+         })
+         break
       }
-   })
-   return [str.slice(i), out]
+   }
+
+   next(spec.shift())
 }
--- stream.js
+++ stream.js
@@ -1,17 +1,64 @@
 /**
  * stream.js - my stream implementation.
+ *
+ * 2010, Michel Beloshitsky
+ *
+ * Licensed under the terms of MIT license. See COPYING file in the
+ * root of distribution.
  */
-exports.make = function (netStr) {
+exports.make = function (netStr, enc) {
+
+   var closed = false, encoding = enc || 'utf8'
 
    var drainCallback = null, readCallback = null, buffer = '', requestedSize = 0
 
+   function checkReceivedEnough () {
+      if (readCallback && buffer.length >= requestedSize) {
+         readCallback(buffer.substr(0, requestedSize))
+         readCallback = null
+         buffer = buffer.slice(requestedSize)
+         requestedSize = 0
+      }
+   }
+
    function write(str, cb) {
-      
+      drainCallback = cb
+      netStr.write(str, encoding)
    }
 
    function read(size, cb) {
-      
+      readCallback = cb
+      requestedSize = size
+      checkReceivedEnough()
    }
 
-   return {read:read, write:write}
+   function close() {
+      closed = true
+      netStr.destroy()
+   }
+
+   /* Event handlers */
+
+   netStr.addListener('connect' , function () {
+      netStr.setEncoding(encoding)
+   })
+
+   netStr.addListener('end' , function () {
+   })
+
+   netStr.addListener('data' , function (data) {
+      buffer += data
+      checkReceivedEnough()
+   })
+
+   netStr.addListener('drain' , function () {
+      if (drainCallback) {
+         drainCallback()
+         drainCallback = null
+      }
+   })
+
+   /* Public interface */
+
+   return {read:read, write:write, close: close}
 }
